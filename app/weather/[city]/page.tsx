90324d4fdc0f7a4a2d3c01ecf906b72277583566
Rohit Patel <rohitpatelmankari@gmail.com>
2025-05-08 22:32:48 +0530

    add promise as a params

--- app/weather/[city]/page.tsx
+++ app/weather/[city]/page.tsx
@@ -2,17 +2,16 @@ import { fetchWeatherByCityName } from '@/app/utils/api';
 import MapComponent from '@/app/components/MapComponent';
 
 interface PageProps {
-  params: {
-    city: string;
-  };
+  params: Promise<{ city: string }>;
 }
 
 const WeatherPage = async ({ params }: PageProps) => {
-  const weather = await fetchWeatherByCityName(params.city);
+  const { city } = await params;
+  const weather = await fetchWeatherByCityName(city);
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-2">Weather in {params.city}</h1>
+      <h1 className="text-2xl font-bold mb-2">Weather in {city}</h1>
       <p className="mb-4">Temperature: {weather.main.temp}°C</p>
       <MapComponent lat={weather.coord.lat} lon={weather.coord.lon} />
     </div>
